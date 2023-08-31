172cfa6aceb45388879973b10e1d6b77587aef83
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-08-31 13:17:13 -0300

    Criadas as rotas para a aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+
 import './styles/App.css';
 import { About, NotFound, Posts, Users } from './pages';
 
 function App() {
-  return (<p>Exercício Bloggers - React Router</p>);
+  return (
+    <>
+      <p>Exercício Bloggers - React Router</p>
+      <Routes>
+        <Route path="/" element={ <Users /> } />
+        <Route path="/posts/:id" element={ <Posts /> } />
+        <Route path="/about" element={ <About /> } />
+        <Route path="/*" element={ <NotFound /> } />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
